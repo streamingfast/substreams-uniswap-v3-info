38260aad836f8f41f0fbd709ec430aaf9ccab9aa
Julien Cassis <juliencassis@gmail.com>
2023-05-24 12:22:02 -0400

    added left padding

--- src/kv_out.rs
+++ src/kv_out.rs
@@ -22,8 +22,7 @@ pub fn pool_day_data_create(ops: &mut KvOperations, tx_count_deltas: &Deltas<Del
     {
         let time_id = key_segment(&delta.key, 1).parse::<i64>().unwrap();
         let pool_address = key_segment(&delta.key, 2);
-        let pool_addr = format!("0x{pool_address}");
-        create_pool_day_data(ops, time_id, &pool_addr, delta.ordinal);
+        create_pool_day_data(ops, time_id, pool_address, delta.ordinal);
     }
 }
 
@@ -39,15 +38,16 @@ pub fn pool_day_data_update(
 fn create_pool_day_data(
     ops: &mut KvOperations,
     date_id: i64,
-    pool_addr: &str,
+    pool_address: &str,
     ordinal: u64,
 ) {
     let date = BigInt::from(date_id * 86400);
-
-    ops.push_new(format!("{pool_addr}:{date_id}:date"), date.to_string(), ordinal);
-    ops.push_new(format!("{pool_addr}:{date_id}:volumeUSD"), BigDecimal::zero().to_string(), ordinal);
-    ops.push_new(format!("{pool_addr}:{date_id}:tvlUSD"), BigDecimal::zero().to_string(), ordinal);
-    ops.push_new(format!("{pool_addr}:{date_id}:feesUSD"), BigDecimal::zero().to_string(), ordinal);
+    let pool_addr = format!("0x{pool_address}");
+    let padded_date_id = left_pad(date_id);
+    ops.push_new(format!("pool:{pool_addr}:{padded_date_id}:date"), date.to_string(), ordinal);
+    ops.push_new(format!("pool:{pool_addr}:{padded_date_id}:volumeUSD"), BigDecimal::zero().to_string(), ordinal);
+    ops.push_new(format!("pool:{pool_addr}:{padded_date_id}:tvlUSD"), BigDecimal::zero().to_string(), ordinal);
+    ops.push_new(format!("pool:{pool_addr}:{padded_date_id}:feesUSD"), BigDecimal::zero().to_string(), ordinal);
 }
 
 
@@ -63,8 +63,11 @@ pub fn swap_volume(ops: &mut KvOperations, swaps_volume_deltas: &Deltas<DeltaBig
         ]))
     {
         let (_, time_id, pool_address) = pool_windows_id_fields(&delta.key);
+        let date_id = time_id.parse::<i64>().unwrap();
         let attribute = last_segment(&delta.key);
-        ops.push_new(format!("0x{pool_address}:{time_id}:{attribute}"),&delta.new_value.to_string(), delta.ordinal);
+        let pool_addr = format!("0x{pool_address}");
+        let padded_date_id = left_pad(date_id);
+        ops.push_new(format!("{pool_addr}:{padded_date_id}:{attribute}"),&delta.new_value.to_string(), delta.ordinal);
     }
 }
 
@@ -77,7 +80,10 @@ pub fn total_value_locked_usd(ops: &mut KvOperations, derived_tvl_deltas: &Delta
         .filter(key_last_segment_in("totalValueLockedUSD"))
     {
         let (_, time_id, pool_address) = pool_windows_id_fields(&delta.key);
-        ops.push_new(format!("0x{pool_address}:{time_id}:tvlUSD"), &delta.new_value.to_string(), delta.ordinal);
+        let date_id = time_id.parse::<i64>().unwrap();
+        let pool_addr = format!("0x{pool_address}");
+        let padded_date_id = left_pad(date_id);
+        ops.push_new(format!("{pool_addr}:{padded_date_id}:tvlUSD"), &delta.new_value.to_string(), delta.ordinal);
     }
 }
 
@@ -105,7 +111,7 @@ fn last_segment(key: &String) -> &str {
     return try_last_segment(key).unwrap();
 }
 
-pub fn try_last_segment(key: &String) -> Option<&str> {
+fn try_last_segment(key: &String) -> Option<&str> {
     let val = key.split(":").last();
     match val {
         Some(val) => Some(val),
@@ -113,18 +119,33 @@ pub fn try_last_segment(key: &String) -> Option<&str> {
     }
 }
 
-pub fn first_segment(key: &String) -> &str {
+fn first_segment(key: &String) -> &str {
     key.split(":").next().unwrap()
 }
 
-pub fn segment(key: &String, index: usize) -> &str {
+fn segment(key: &String, index: usize) -> &str {
     return try_segment(key, index).unwrap();
 }
 
-pub fn pool_windows_id_fields(key: &String) -> (&str, &str, &str) {
+fn pool_windows_id_fields(key: &String) -> (&str, &str, &str) {
     let table_name = first_segment(key);
     let time_id = segment(key, 1);
     let pool_address = segment(key, 2);
 
     return (table_name, time_id, pool_address);
+}
+
+fn left_pad(key: i64) -> String {
+    return format!("{:0>10}", key)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_left_pad() {
+
+        assert_eq!("0000000123", left_pad(123));
+    }
 }
