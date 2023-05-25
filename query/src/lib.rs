834e0ef390bc9c3d9c2d94f047f337a41e6c735c
Matthieu Vachon <matt@streamingfast.io>
2023-05-25 13:26:45 -0400

    Removed timing print statements

--- query/src/lib.rs
+++ query/src/lib.rs
@@ -6,7 +6,6 @@ use prost::Message;
 use std::collections::HashMap;
 use std::str;
 use std::str::FromStr;
-use std::time::SystemTime;
 use substreams_sink_kv::prelude::*;
 
 #[allow(unused_imports)]
@@ -26,27 +25,17 @@ pub fn uniswap_info_v1_uniswapinfo_pooldaydatas(v: Vec<u8>) -> Result<Vec<u8>, S
 }
 
 pub fn handler(v: Vec<u8>) -> Result<Vec<u8>, String> {
-    let now = SystemTime::now();
-
     let req = PoolDayDatasRequest::decode(&v[..]).expect("Failed to decode");
     let store = Store::new();
 
-    let mut accum = HashMap::<String, PoolDayData>::new();
+    let mut accum = HashMap::<String, PoolDayData>::with_capacity(20 * 1000);
 
     for pool_addr in req.addresses {
-        let now_pool = SystemTime::now();
-
         let start = format!("PoolDayData:{}:0000000000", pool_addr);
         let end = format!("PoolDayData:{}:9999999999", pool_addr);
 
-        let now_scan = SystemTime::now();
         let key_values = store.scan(start, end, None);
-
         for kv_pair in key_values.pairs {
-            // if pool_addr == "0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640" {
-            //     println!("key {:?}", kv_pair.key);
-            // }
-
             let Key { data_type, date_id } = split_key(&kv_pair.key);
             let value = f64::from_str(unsafe { str::from_utf8_unchecked(kv_pair.value.as_slice()) }).unwrap();
             let elem: &mut PoolDayData = accum.entry(date_id.to_string()).or_default();
@@ -57,13 +46,9 @@ pub fn handler(v: Vec<u8>) -> Result<Vec<u8>, String> {
                 _ => (),
             }
         }
-
-        println!("key transformed in {:?}", now_transform.elapsed().unwrap());
-        println!("pool took {:?}", now_pool.elapsed().unwrap());
     }
 
-    let now_out = SystemTime::now();
-    let mut out = PoolDayDatasResponse {
+    let mut out: PoolDayDatasResponse = PoolDayDatasResponse {
         pool_days_data: accum
             .into_iter()
             .map(|(key, val)| PoolsDayData {
@@ -73,13 +58,8 @@ pub fn handler(v: Vec<u8>) -> Result<Vec<u8>, String> {
             })
             .collect(),
     };
-    println!("out done in {:?}", now_out.elapsed().unwrap());
 
-    let now_sort = SystemTime::now();
     out.pool_days_data.sort_by(|a, b| a.date.cmp(&b.date));
-    println!("sort done in {:?}", now_sort.elapsed().unwrap());
-
-    println!("done in {:?}", now.elapsed().unwrap());
 
     return Ok(out.encode_to_vec());
 }
